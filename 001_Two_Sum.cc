131ac8e609aacd578a9e4a57636b827eb2922405
kekenow <zhongke@live.com>
2016-05-03 22:48:58 +0800

    ADD : First algo

---
+++ 001_Two_Sum.cc
@@ -0,0 +1,41 @@
+/* Given an array of integers, return indices of the two numbers such that they
+   add up to a specific target. */
+
+#include <isotream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+/*
+ * Check if the array has duplicated numbers
+ * 
+ */
+
+//int* getSum(int[], )
+
+
+int main()
+{
+        int array[] = {2, 7, 11, 15};
+
+/*
+        vector<int> i_vec = {1, 3, 7, 10, 21, 5, 8, 9, 24};
+
+        for (iterator<int> it = i_vec.begin(); it != i_vec.end(); ++it) {
+
+        }
+*/
+        for (int i = 0; i < 4; i++) {
+                for (int j = i; j < 4; j++) {
+                        
+                }
+
+        }
+
+
+
+
+
+
+        return 0;
+}
