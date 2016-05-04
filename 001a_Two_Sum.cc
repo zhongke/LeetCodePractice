3622da03e45b343bd8d8797bd8a1af0cb429d5d1
Kevin Zhong <kekenow@163.com>
2016-05-04 13:23:56 +0800

    ADD: 006a file

--- 001a_Two_Sum.cc
+++ 001a_Two_Sum.cc
@@ -1,5 +1,21 @@
-/* Given an array of integers, return indices of the two numbers such that they
-   add up to a specific target. */
+/**
+ * [Title]
+ * 001a Two Sum
+ *
+ * [Difficulty]
+ * Easy
+ *
+ * [Description]
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * You may assume that each input would have exactly one solution.
+ *
+ * [Example]
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
 
 #include <iostream>
 #include <vector>
--- 002b_Add_Two_Numbers.cc
+++ 002b_Add_Two_Numbers.cc
@@ -1,6 +1,18 @@
-/* You are given two linked lists representing two non-negative numbers. The
+/**
+ * [Title]
+ * 002b Add Two Numbers
+ *
+ * [Difficulty]
+ * Medium
+ *
+ * [Description]
+ * You are given two linked lists representing two non-negative numbers. The
  * digits are stored in reverse order and each of their nodes contain a single
  * digit. Add the two numbers and return it as a linked list.
+ *
+ * [Example]
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
  */
 
 #include <iostream>
