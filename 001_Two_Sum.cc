817c640a5fffc860b8077d3c946a59c3785afff6
Kevin Zhong <kekenow@163.com>
2016-05-04 05:33:45 +0100

    Update first proposal

--- 001_Two_Sum.cc
+++ 001_Two_Sum.cc
@@ -1,41 +1,48 @@
 /* Given an array of integers, return indices of the two numbers such that they
    add up to a specific target. */
 
-#include <isotream>
+#include <iostream>
 #include <vector>
 #include <iterator>
 
 using namespace std;
 /*
  * Check if the array has duplicated numbers
- * 
+ * Check if there are one more matched group
  */
-
-//int* getSum(int[], )
-
 
 int main()
 {
-        int array[] = {2, 7, 11, 15};
+        int array[] = {15, 7, 9, 2};
+        int i_number1 = 0, i_number2 = 0, i_sum = 0;
+        int index[] = {0, 0};
 
-/*
-        vector<int> i_vec = {1, 3, 7, 10, 21, 5, 8, 9, 24};
+        bool matched = false;
 
-        for (iterator<int> it = i_vec.begin(); it != i_vec.end(); ++it) {
 
-        }
-*/
         for (int i = 0; i < 4; i++) {
-                for (int j = i; j < 4; j++) {
-                        
+                for (int j = i + 1; j < 4; j++) {
+                        for (int k = 0; k < 4; k++) {
+
+                                if ((k != i && k != j) && (array[i] + array[j] == array[k])) {
+                                        matched = true;
+                                        i_number1 = i;
+                                        i_number2 = j;
+                                        i_sum = k;
+                                        cout << "i_number1 : " << i_number1 << " : " << array[i] << endl;
+                                        cout << "i_number2 : " << i_number2 << " : " << array[j] << endl;
+                                        cout << "i_sum     : " << k         << " : " << array[k] << endl;
+
+                                }
+
+                                if (matched) break;
+                        }
+
+                        if (matched) break;
                 }
 
+                if (matched) break;
         }
-
-
-
-
-
 
         return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # LeetCodePractice
 LeetCodePractice
+
+Try to provide the solution for all the challenges in the link below:
+
+https://leetcode.com/problemset/algorithms/
+
+Any comments are welcome! :-)
+
+BRs
+Kevin
+
